80855f86d8b3e980e1cca54152a02a3f9bd6ea0a
JGBMichalski <31932269+JGBMichalski@users.noreply.github.com>
2019-01-14 17:57:33 -0600

    Initial Upload

---
+++ JetsonServer/src/jetsonserver/JetsonServer.java
@@ -0,0 +1,36 @@
+package jetsonserver;
+
+import java.io.*;
+import java.net.Socket;
+
+public class JetsonServer {
+     static Socket pingSocket = null;
+     static PrintWriter out = null;
+     static BufferedReader in = null;
+
+    public static void send(String x) throws IOException {
+        out.println(x);
+    }
+    
+    public static boolean connect(){
+        try {
+            pingSocket = new Socket("10.42.0.121", 23);
+            out = new PrintWriter(pingSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(pingSocket.getInputStream()));
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+    
+    public static boolean disconnect(){
+        try {
+            out.close();
+            in.close();
+            pingSocket.close();
+            return true;
+        } catch (IOException e){
+            return false;
+        }
+    }
+}
