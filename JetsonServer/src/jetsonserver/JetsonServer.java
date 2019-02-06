bcaf3697bbe82be88bff069e1d750514bb760b72
JGBMichalski <JGBMichalski@users.noreply.github.com>
2019-02-06 11:23:01 -0600

    Added Sentry Mode
    - Added sentry mode in JetsonServer.java and made it run on its own thread.
    - Simplified clientConnection.java and added ability to start/stop sentry mode.

--- JetsonServer/src/jetsonserver/JetsonServer.java
+++ JetsonServer/src/jetsonserver/JetsonServer.java
@@ -2,17 +2,24 @@ package jetsonserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
-public class JetsonServer {
-     static Socket pingSocket = null;
-     static PrintWriter out = null;
-     static BufferedReader in = null;
+public class JetsonServer implements Runnable {
+     private Socket pingSocket = null;
+     private PrintWriter out = null;
+     private BufferedReader in = null;
+     private boolean sentMode = false;
+     private gui gui;
+     
+     public JetsonServer(gui x){
+         gui = x;
+     }
 
-    public static void send(String x) throws IOException {
+    public void send(String x) throws IOException {
         out.println(x + ";");
     }
     
-    public static boolean connect(){
+    public boolean connect(){
         try {
             pingSocket = new Socket("10.42.0.121", 23);
             out = new PrintWriter(pingSocket.getOutputStream(), true);
@@ -23,7 +30,7 @@ public class JetsonServer {
         }
     }
     
-    public static boolean disconnect(){
+    public boolean disconnect(){
         try {
             out.close();
             in.close();
@@ -32,5 +39,27 @@ public class JetsonServer {
         } catch (IOException e){
             return false;
         }
+    }
+    
+    //Sentry Mode
+    public void run(){
+        while (true){
+            if (sentMode){
+                try{
+                    send("x+999");
+                    Thread.sleep(15000);
+                    send("x-999");
+                    Thread.sleep(15000);
+                } catch (Exception e){}
+            } else {
+                try{
+                    Thread.sleep(1000);
+                } catch (Exception e){}
+            }
+        }
+    }
+    
+    public void setMode(boolean x){
+        sentMode = x;
     }
 }
--- JetsonServer/src/jetsonserver/clientConnection.java
+++ JetsonServer/src/jetsonserver/clientConnection.java
@@ -10,38 +10,45 @@ public class clientConnection{
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
-    private String aqon = "gst-launch-1.0 v4l2src device=/dev/video1 ! "
-            + "video/x-raw, width=3840, height=1080 ! videocrop top=0 left=0 "
-            + "right=1920 bottom=0 ! omxh264enc control-rate=2! tee name=t ! "
-            + "queue ! video/x-h264, stream-format=byte-stream ! h264parse ! "
-            + "rtph264pay ! udpsink host=140.193.207.255 port=5000 t. ! queue";
-    private String aqoff = "";
-    private String deton1 = "/home/nvidia/zed-yolo/zed_cpp_sample/build/darknet_zed /home/nvidia/zed-yolo/libdarknet/data/coco.names "
-            + "/home/nvidia/zed-yolo/libdarknet/cfg/yolov3-tiny.cfg /home/nvidia/zed-yolo/libdarknet/yolov3-tiny.weights";
-    private String deton2 = "gst-launch-1.0 ximagesrc xname=\"ZED\" use-damage=0 ! "
-            + "video/x-raw ! timeoverlay ! queue ! videoconvert ! omxh264enc "
-            + "control-rate=2 ! tee name=t ! queue ! video/x-h264, "
-            + "stream-format=byte-stream ! h264parse ! rtph264pay ! "
-            + "udpsink host=140.193.230.117 port=5000 t. ! "
-            + "queue ! mpegtsmux ! filesink location=both.mp4 -e";
-    private String detoff= "killall -9 /home/nvidia/zed-yolo/zed_cpp_sample/build/darknet_zed";
-
     static boolean isConnected = false;
     static String cmd = "";
     static gui gui;
     static clientConnection server;
+    static JetsonServer JT;
+    private String aqon;
+    private String aqoff;
+    private String deton1;
+    private String deton2;
+    private String detoff1;
+    private String detoff2;
+    
+    
 
     public static void main(String[] args) {
+        // Setting up clientConnection.java
         server = new clientConnection();
+        server.loadVariables();
+        
+        // Setting up gui.java
         gui = new gui(server);
         gui.setVisible(true);
-        JetsonServer.connect();
+        
+        // Setup JetsonServer.java and create its thread
+        JT = new JetsonServer(gui);
+        Thread t = new Thread(JT);
+        t.start();
+        //JT.connect();
+        
+        // Initiate the server to connect to web client
         server.start(8080);
+        
+        // If connected, listen for input from web client
         while (isConnected = true){
             cmd = server.listen();
         }
     }
 
+    // Setup socket and wait for client
     public Socket start(int port){
         try {
             gui.display("Setting up socket...");
@@ -53,11 +60,12 @@ public class clientConnection{
             gui.display("Connected to client");
             out = new PrintWriter(clientSocket.getOutputStream(), true);
         } catch (IOException e){
-            e.printStackTrace();
+            gui.display("Could not setup socket. Please restart.");
         } 
         return clientSocket;
     }
 
+    // Close all connections
     public void stop(){
         try {
             in.close();
@@ -71,6 +79,7 @@ public class clientConnection{
         isConnected = false;
     }
 
+    //Listen for input from web client
     public String listen(){
         String msg = new String();
         try {
@@ -85,27 +94,12 @@ public class clientConnection{
         }
         return cmd;
     }
-
-    public void executeCMD(String CMD) {
-        String s;
-        Process p;
-        try {
-            p = Runtime.getRuntime().exec(CMD);
-            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((s = br.readLine()) != null)
-                gui.display(s);
-            p.waitFor();
-            System.out.println (p.exitValue());
-            p.destroy();
-        } catch (Exception e) {
-            gui.display("Could not execute command: " + CMD);
-        }
-    }
   
+    // Execute any command sent from the web client
     public void handleCmd(String x) throws IOException{
         if (x.contains("x+") || x.contains("x-") || x.contains("y+") || x.contains("y-")){
             try {
-                JetsonServer.send(x);
+                JT.send(x);
                 gui.display("Passed '" + x + "' to Arduino.");
                 //out.println("Passed '" + x + "' to Arduino.");
             } catch (Exception e){
@@ -124,11 +118,13 @@ public class clientConnection{
             //out.println("Entering Aquisition mode...");
             gui.display("Entering Aquisition mode...");
             execLinCmd(aqon);
+            JT.setMode(true);
         } else if (x.equals("aqoff")){
             //Turn off Data Aquisition mode
             //out.println("Stopping Aquisition mode...");
             gui.display("Stopping Aquisition mode...");
             execLinCmd(aqoff);
+            JT.setMode(false);
         } else if (x.equals("deton")){
             //Turn on Detection mode
             //out.println("Entering Detection mode...");
@@ -149,12 +145,14 @@ public class clientConnection{
             //Turn off Detection mode
             //out.println("Stopping Detection mode...");
             gui.display("Stopping Detection mode...");
-            execLinCmd(detoff);
-        } else if (x.equals("test")){
-            gui.display("'test' has been entered.");
+            execLinCmd(detoff1);
+            execLinCmd(detoff2);
+        } else {
+            gui.display("Invalid command: " + x);
         }
     }
     
+    // Execute any linux command sent by the web server
     private void execLinCmd(String cmd){
         String s = null;
         
@@ -164,5 +162,33 @@ public class clientConnection{
         catch (Exception e) {
             gui.display("Error: " + e);
         }
+    }
+    
+    // Load the variables for executing commands
+    private void loadVariables(){
+        // Launches Gstreamer and streams to browser on localhost
+        aqon = "gst-launch-1.0 v4l2src device=/dev/video1 ! "
+                    + "video/x-raw, width=3840, height=1080 ! videocrop top=0 left=0 "
+                    + "right=1920 bottom=0 ! omxh264enc control-rate=2! tee name=t ! "
+                    + "queue ! video/x-h264, stream-format=byte-stream ! h264parse ! "
+                    + "rtph264pay ! udpsink host=localhost port=5000 t. ! queue";
+        // Kills Gstreamer process
+        aqoff = "killall -9 gstreamer";
+        // Launches YOLOv3
+        deton1 = "/home/nvidia/zed-yolo/zed_cpp_sample/build/darknet_zed "
+                    + "/home/nvidia/zed-yolo/libdarknet/data/coco.names /home/nvidia/"
+                    + "zed-yolo/libdarknet/cfg/yolov3-tiny.cfg /home/nvidia/zed-yolo/"
+                    + "libdarknet/yolov3-tiny.weights";
+        // Launches Gstreamer to stream YOLO to browser
+        deton2 = "gst-launch-1.0 ximagesrc xname=\"ZED\" use-damage=0 ! "
+                    + "video/x-raw ! timeoverlay ! queue ! videoconvert ! omxh264enc "
+                    + "control-rate=2 ! tee name=t ! queue ! video/x-h264, "
+                    + "stream-format=byte-stream ! h264parse ! rtph264pay ! "
+                    + "udpsink host=140.193.230.117 port=5000 t. ! "
+                    + "queue ! mpegtsmux ! filesink location=both.mp4 -e";
+        // Kills YOLOv3 process
+        detoff1= "killall -9 /home/nvidia/zed-yolo/zed_cpp_sample/build/darknet_zed";
+        // Kills Gstreamer process
+        detoff2= "killall -9 gstreamer";
     }
 }
